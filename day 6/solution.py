1e1240afccc40a33e23062522d488215023ce0ae
Jan D <jandohring@hotmail.com>
2022-12-06 13:57:26 -0500

    day 6

---
+++ day 6/solution.py
@@ -0,0 +1,12 @@
+with open('day 6/input.txt') as file:
+    string = file.readline()
+
+    for amt in (4, 14):
+        i = 0
+        for i in range(len(string)):
+            if len(set(string[i:i+amt])) == amt:
+                break
+        print(i + amt)
+    
+    # 1282
+    # 3513
